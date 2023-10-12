d6c114e702761bb2e482e48b7b929856410d6ccb
rohitGupta2022 <pta.rohit28@gmail.com>
2023-10-12 11:21:05 +0530

    Add: Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Link from 'next/link'
+import { LinkedinIcon, LucideGithub } from 'lucide-react'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -29,10 +30,26 @@ export default function RootLayout({
               <Link href="/">Home</Link>
               <Link href="/pricing">Pricing</Link>
               <Link href="mailto:pta.rohit28@gmail.com">Contact</Link>
-              <Link href="https://github.com/rohitt-gupta/caption-me">Github</Link>
+              <Link href="https://github.com/rohitt-gupta/caption-me" className="after:content-['_Star on github']">Github</Link>
             </nav>
           </header>
           {children}
+          <footer className='p-4 pb-2 max-w-2xl mx-auto '>
+            <div className='flex justify-between my-2 sm:my-8'>
+              <h4>
+                Build by <Link className='text-black' href='https://twitter.com/whyrohitwhy'>Rohit</Link>, with the help of <Link className='text-black' href='https://nextjs.org/'>Nextjs</Link>
+              </h4>
+              <div className='flex ic jc gap-2'>
+                <Link href='https://www.linkedin.com/in/rohit-gupta28/'>
+                  <LinkedinIcon className='w-5 h-5 text-white' />
+                </Link>
+                <Link href='https://github.com/rohitt-gupta/caption-me'>
+                  <LucideGithub className='w-5 h-5 text-white' />
+                </Link>
+              </div>
+            </div>
+            <h5>If you like Caption Me, Please give it a star on <Link href="https://github.com/rohitt-gupta/caption-me" className="after:content-['_Star on github'] text-zinc-700 font-bold">Github</Link></h5>
+          </footer>
         </main>
       </body>
     </html>
